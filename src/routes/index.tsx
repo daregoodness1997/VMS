1ac749e3cede0a1a24e0e2a8a14530ba4f4e56c9
daregoodness1997 <daregoodness@gmail.com>
2022-04-14 19:21:26 +0100

    added netlify.toml

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import SignIn from '../pages/auth/sign-in';
-import Dashboard from '../pages/app/Dashboard';
 import { RequireAuth } from './requireAuth';
 import SignUp from '../pages/auth/sign-up';
 import Overview from '../core-ui/Dashboard/Overview';
